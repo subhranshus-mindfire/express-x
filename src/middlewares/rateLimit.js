f36143a873e966193b804de402b3dc29ed9d9d1e
subhranshu <subhranshus@mindfiresolutions.com>
2025-09-07 05:22:56 +0530

    fix: fix the nested key issue in ratelimit.js

--- src/middlewares/rateLimit.js
+++ src/middlewares/rateLimit.js
@@ -2,8 +2,8 @@ const rateLimit = require("express-rate-limit");
 
 module.exports = (app, config = {}) => {
   const limiter = rateLimit({
-    windowMs: (config.windowMs || 15) * 60 * 1000,
-    max: config.max || 100,
+    windowMs: (config.rateLimit.windowMs || 15) * 60 * 1000,
+    max: config.rateLimit.max || 100,
     message: config.message || "Too many requests, please try again later.",
   });
 
